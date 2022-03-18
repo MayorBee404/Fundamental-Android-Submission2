5e7a81f16f102be290d8a7c28fb70bfdafe5b3dd
MayorBee404 <bagss69@gmail.com>
2022-03-18 18:40:34 +0700

    Revisi ProgressBar lebih baik

--- app/src/main/java/com/dicoding/ittelkom/githubuser/network/ApiServices.kt
+++ app/src/main/java/com/dicoding/ittelkom/githubuser/network/ApiServices.kt
@@ -7,28 +7,28 @@ import retrofit2.http.*
 interface ApiServices {
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetailUser(
         @Path("username")
         login: String
     ): Call<DetailResource>
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun searchUsers (
         @Query("q")
         query: String
     ): Call<SearchUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUserFollowers (
         @Path("username")
         login: String
     ): Call<List<DetailResource>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUserFollowing (
         @Path("username")
         login: String
